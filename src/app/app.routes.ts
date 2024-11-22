4013a269d309028910d58d2a93f6d72e2090aeb2
fitriaulianisa <aulianisafitri434@gmail.com>
2024-11-22 10:58:28 +0700

    update
    menambahkan component prodi dan mahasiswa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { FakultasComponent } from './components/fakultas/fakultas.component';
-
+import { ProdiComponent } from './components/prodi/prodi.component';
+import { MahasiswaComponent } from './components/mahasiswa/mahasiswa.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'fakultas', component: FakultasComponent }
+    { path: 'fakultas', component: FakultasComponent },
+    { path: 'prodi', component: ProdiComponent },
+    { path: 'mahasiswa', component: MahasiswaComponent }
 ];
 
---
+++ src/app/components/prodi/prodi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prodi',
+  standalone: true,
+  imports: [],
+  templateUrl: './prodi.component.html',
+  styleUrl: './prodi.component.css'
+})
+export class ProdiComponent {
+
+}
