ba3998a4bbeaf174f6a7844c9ced28b274d72f7d
fitriaulianisa <aulianisafitri434@gmail.com>
2024-11-22 10:41:23 +0700

    belajar-angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FakultasComponent } from './components/fakultas/fakultas.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'fakultas', component: FakultasComponent }
+];
+
---
+++ src/app/components/fakultas/fakultas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fakultas',
+  standalone: true,
+  imports: [],
+  templateUrl: './fakultas.component.html',
+  styleUrl: './fakultas.component.css'
+})
+export class FakultasComponent {
+
+}
