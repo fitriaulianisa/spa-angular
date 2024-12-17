7dc85bc46d7b4df88d1d81ee5365dac6133fb837
fitriaulianisa <aulianisafitri434@gmail.com>
2024-12-17 11:16:42 +0700

    update prodi.component.ts

--- src/app/components/prodi/prodi.component.ts
+++ src/app/components/prodi/prodi.component.ts
@@ -115,7 +115,9 @@ export class ProdiComponent implements OnInit { // Mendeklarasikan class kompone
   // Method untuk menghapus prodi
 deleteProdi(_id: string): void {
   if (confirm('Apakah Anda yakin ingin menghapus data ini?')) { // Konfirmasi penghapusan
-    this.http.delete(`${this.apiProdiUrl}/${_id}`).subscribe({
+    const token = localStorage.getItem('authToken'); //ambil token dari localstorage
+    const headers = { Authorization: `Bearer ${token}`};
+    this.http.delete(`${this.apiProdiUrl}/${_id}`, { headers}).subscribe({
       next: () => {
         console.log(`Prodi dengan ID ${_id} berhasil dihapus`);
         this.getProdi(); // Refresh data prodi setelah penghapusan
